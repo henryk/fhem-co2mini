3c32c0fe05ff363fd8d1c59c4b090618d222198a
Henryk Plötz <henryk@ploetzli.ch>
2015-10-03 20:54:01 +0200

    Add fully functioning code

--- FHEM/38_co2mini.pm
+++ FHEM/38_co2mini.pm
@@ -4,17 +4,23 @@ package main;
 use strict;
 use warnings;
 
+use Fcntl;
+
+# Key retrieved from /dev/random, guaranteed to be random ;-)
+my $key = "u/R\xf9R\x7fv\xa5";
+
 sub
 co2mini_Initialize($)
 {
   my ($hash) = @_;
 
   $hash->{DefFn}    = "co2mini_Define";
+  $hash->{ReadFn}   = "co2mini_Read";
   $hash->{NOTIFYDEV} = "global";
   $hash->{NotifyFn} = "co2mini_Notify";
   $hash->{UndefFn}  = "co2mini_Undefine";
   $hash->{AttrFn}   = "co2mini_Attr";
-  $hash->{AttrList} = "disable:1 ".
+  $hash->{AttrList} = "disable:0,1 showraw:0,1 ".
                       $readingFnAttributes;
 }
 
@@ -64,7 +70,18 @@ co2mini_Connect($)
 
   return undef if( AttrVal($name, "disable", 0 ) == 1 );
 
-  # FIXME Implement
+  sysopen($hash->{HANDLE}, $hash->{DEVICE}, O_RDWR | O_APPEND | O_NONBLOCK) or return undef;
+
+  # Result of printf("0x%08X\n", HIDIOCSFEATURE(9)); in C
+  my $HIDIOCSFEATURE_9 = 0xC0094806;
+
+  # Send a FEATURE Set_Report with our key
+  ioctl($hash->{HANDLE}, $HIDIOCSFEATURE_9, "\x00".$key) or return undef;
+
+  $hash->{FD} = fileno($hash->{HANDLE});
+  $selectlist{"$name"} = $hash;
+
+  $hash->{STATE} = "connecting";
 
 }
 
@@ -74,10 +91,81 @@ co2mini_Disconnect($)
   my ($hash) = @_;
   my $name   = $hash->{NAME};
 
-  # FIXME Implement
+  if($hash->{HANDLE}) {
+    delete $selectlist{"$name"};
+    delete $hash->{FD};
+
+    close($hash->{HANDLE});
+    delete $hash->{HANDLE};
+  }
 
   $hash->{STATE} = "disconnected";
   Log3 $name, 3, "$name: disconnected";
+}
+
+
+# Input: string key, string data
+# Output: array of integers result
+sub
+co2mini_decrypt($$)
+{
+  my ($key_, $data_) = @_;
+  my @key = map { ord } split //, $key_;
+  my @data = map { ord } split //, $data_;
+  my @cstate = (0x48,  0x74,  0x65,  0x6D,  0x70,  0x39,  0x39,  0x65);
+  my @shuffle = (2, 4, 0, 7, 1, 6, 5, 3);
+  
+  my @phase1 = (0..7);
+  for my $i (0 .. $#phase1) { $phase1[ $shuffle[$i] ] = $data[$i]; }
+  
+  my @phase2 = (0..7);
+  for my $i (0 .. 7) { $phase2[$i] = $phase1[$i] ^ $key[$i]; }
+  
+  my @phase3 = (0..7);
+  for my $i (0 .. 7) { $phase3[$i] = ( ($phase2[$i] >> 3) | ($phase2[ ($i-1+8)%8 ] << 5) ) & 0xff; }
+  
+  my @ctmp = (0 .. 7);
+  for my $i (0 .. 7) { $ctmp[$i] = ( ($cstate[$i] >> 4) | ($cstate[$i]<<4) ) & 0xff; }
+  
+  my @out = (0 .. 7);
+  for my $i (0 .. 7) { $out[$i] = (0x100 + $phase3[$i] - $ctmp[$i]) & 0xff; }
+  
+  return @out;
+}
+
+sub
+co2mini_Read($)
+{
+  my ($hash) = @_;
+  my $name   = $hash->{NAME};
+  my ($buf, $readlength);
+
+  my $showraw = AttrVal($name, "showraw", 0);
+
+  readingsBeginUpdate($hash);
+  while ( defined($readlength = sysread($hash->{HANDLE}, $buf, 8)) and $readlength == 8 ) {
+    my @data = co2mini_decrypt($key, $buf);
+    Log3 $name, 5, "co2mini data received " . join(" ", @data);
+    # FIXME Verify checksum
+
+    my ($item, $val_hi, $val_lo, $rest) = @data;
+    my $value = $val_hi << 8 | $val_lo;
+    
+    if($item == 0x50) {
+      readingsBulkUpdate($hash, "co2", $value);
+    } elsif($item == 0x42) {
+      readingsBulkUpdate($hash, "temperature", $value/16.0 - 273.15);
+    } elsif($item == 0x44) {
+      readingsBulkUpdate($hash, "humidity", $value/100.0);
+    }
+    if($showraw) {
+      readingsBulkUpdate($hash, sprintf("raw_%02X", $item), $value);
+    }
+    
+    $hash->{STATE} = "connected";
+  }
+  readingsEndUpdate($hash, 1);
+
 }
 
 sub
